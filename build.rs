cc7abb214833080242fafb7cb3f25e573b43a34b
Chance <jchancehud@gmail.com>
2024-06-25 15:04:48 -0700

    fix: remove uniffi build from app build script

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
-use mopro_ffi::app_config::{build_uniffi, install_archs};
+use mopro_ffi::app_config::install_archs;
 
 fn main() {
     rust_witness::transpile::transpile_wasm("./test-vectors/circom".to_string());
     install_archs();
-    build_uniffi();
+    // uniffi is built/linked in the mopro-ffi build, no need to do it here
 }
