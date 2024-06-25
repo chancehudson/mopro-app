362cf70a006ed0971397f1acfbb5dfd3b4921757
Chance <jchancehud@gmail.com>
2024-06-25 15:16:01 -0700

    uniffi, update

--- build.rs
+++ build.rs
@@ -1,7 +1,8 @@
-use mopro_ffi::app_config::install_archs;
+use mopro_ffi::app_config::{build_uniffi, install_archs};
 
 fn main() {
     rust_witness::transpile::transpile_wasm("./test-vectors/circom".to_string());
     install_archs();
     // uniffi is built/linked in the mopro-ffi build, no need to do it here
+    build_uniffi();
 }
