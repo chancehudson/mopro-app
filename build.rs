1d35cd963897d025002fbd6f59d3ad38856f2356
Chance <jchancehud@gmail.com>
2024-06-26 03:12:06 -0700

    feat: ios project

--- build.rs
+++ build.rs
@@ -1,8 +1,5 @@
-use mopro_ffi::app_config::{build_uniffi, install_archs};
-
 fn main() {
     rust_witness::transpile::transpile_wasm("./test-vectors/circom".to_string());
-    install_archs();
-    // uniffi is built/linked in the mopro-ffi build, no need to do it here
-    build_uniffi();
+    std::fs::write("./src/mopro.udl", mopro_ffi::app_config::UDL).expect("Failed to write UDL");
+    uniffi::generate_scaffolding("./src/mopro.udl").unwrap();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,12 @@
-use mopro_ffi::app;
-use mopro_ffi::circom::WtnsFn;
-use std::path::Path;
+use mopro_ffi::{app, WtnsFn};
 
 rust_witness::witness!(multiplier3);
 
-pub fn circuit_data(zkey_path: &str) -> Result<WtnsFn, MoproError> {
-    let name = Path::new(zkey_path).file_stem().unwrap();
-    match name.to_str().unwrap() {
+app!();
+
+fn circuit_data(name: &str) -> Result<WtnsFn, MoproError> {
+    match name {
         "multiplier3_final" => Ok(multiplier3_witness),
         _ => Err(MoproError::CircomError("Unknown circuit name".to_string())),
     }
 }
-
-app!();
